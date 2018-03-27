a9bbafdf03bfcfb5ea5cb56e7c5cce560f1df64c
viethungtsdv <hung.nguyenviet@toshiba-tsdv.com>
2018-03-27 14:01:33 +0700

    Update example face_api of Microsoft

--- capture_video.py
+++ capture_video.py
@@ -1,14 +1,19 @@
 import numpy as np
 import cv2
 from xlwt import Workbook
+
 #load model face detect của opencv
-face_cascade = cv2.CascadeClassifier('/home/bkic611/Downloads/bkic611/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('/home/bkic611/Downloads/bkic611/lib/python3.6/site-packages/cv2/data/haarcascade_eye.xml')
+#face_cascade = cv2.CascadeClassifier('/home/bkic611/Downloads/bkic611/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+#eye_cascade = cv2.CascadeClassifier('/home/bkic611/Downloads/bkic611/lib/python3.6/site-packages/cv2/data/haarcascade_eye.xml')
+
+# Load model Haar-cascade Detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 #load video
-cap = cv2.VideoCapture('/home/bkic611/Desktop/test/bkopenday.avi')
+cap = cv2.VideoCapture('./bkopenday.avi')
 
-#tao file excel de luu so thu tu cua cac frame co mat nguoi 
+# Create a file excel to save the index of frame have face.
 detected = Workbook()
 detected_s1 = detected.add_sheet("Sheet1")
 detected_s1.write(0,0,'frames')
@@ -20,12 +25,12 @@ while True:
     ret, img = cap.read()
     if ret == True:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #goi ham detect face
+        # Call detect function
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        
-        #neu detect duoc mat thi luu anh vao raw_img va luu chi so frame va file excel
+
+        # If detect have face, then save images into raw_img and the frame index into excel file
         if(len(faces) > 0):
-            frame_indx = cap.get(1)
+            frame_indx = cap.get(1) # Get frame index
             detected_s1.write(index + 1, 0, frame_indx)
             index = index + 1
             name = '/home/bkic611/Dropbox/Computer_vision_bkic/face_detect/rawimg/' + 'ch4_fr%d' % frame_indx + '.png'
